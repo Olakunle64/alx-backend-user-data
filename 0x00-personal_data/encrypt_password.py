3f07283d70fdb80611b13e8714a3420a40f273b6
Olakunle64 <olakunleisiaq50@gmail.com>
2024-05-16 22:03:15 +0100

    use bcrypt to encrypt user's password

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""This module has a function for encrypting user password"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """
+    Hashes a password using bcrypt and returns the hashed password.
+
+    Args:
+        password (str): The password to be hashed.
+
+    Returns:
+        str: The hashed password.
+
+    """
+    hashed = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Check if hashed password is the same as the original
+        password
+
+        Return true if they are the same otherwise false
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
--- 0x00-personal_data/test.py
+++ 0x00-personal_data/test.py
@@ -1,13 +1,17 @@
 import mysql.connector
 import os
 
-db_connector = mysql.connector.connect(
-host="localhost",
-# database=os.getenv('PERSONAL_DATA_DB_NAME', 'holberton'),
-user="root",
-password="")
+# db_connector = mysql.connector.connect(
+# host="localhost",
+# # database=os.getenv('PERSONAL_DATA_DB_NAME', 'holberton'),
+# user="root",
+# password="")
 
-cursor = db_connector.cursor()
-cursor.execute("SELECT COUNT(*) FROM users;")
-print(cursor.fetchone()[0])
-cursor.close()
+# cursor = db_connector.cursor()
+# cursor.execute("SELECT COUNT(*) FROM users;")
+# print(cursor.fetchone()[0])
+# cursor.close()
+
+
+name = "olakunle"
+print(name.encode())

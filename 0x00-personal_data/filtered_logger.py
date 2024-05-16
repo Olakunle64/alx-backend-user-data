c6fadc2b3b03be8755a0762a4cc5c685574972a6
Olakunle64 <olakunleisiaq50@gmail.com>
2024-05-16 18:17:15 +0100

    connect to mysql database from python

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,6 +6,10 @@
 from typing import List
 import re
 import logging
+import mysql.connector
+import os
+
+PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
 
 
 def filter_datum(
@@ -17,3 +21,42 @@ def filter_datum(
         message = re.sub(rf'{field}=.*?{separator}',
                          f'{field}={redaction}{separator}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """This function returns the log message obfuscated"""
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    """This function returns a logger"""
+    logger = logging.getLogger('user_data')
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    stream = logging.StreamHandler()
+    stream.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(stream)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """This function returns a connector to a database"""
+    db_connector = mysql.connector.connect(
+        host=os.getenv('PERSONAL_DATA_DB_HOST', 'localhost'),
+        database=os.getenv('PERSONAL_DATA_DB_NAME', 'holberton'),
+        user=os.getenv('PERSONAL_DATA_DB_USERNAME', 'root'),
+        password=os.getenv('PERSONAL_DATA_DB_PASSWORD', ''))
+    return db_connector
--- 0x00-personal_data/main.py
+++ 0x00-personal_data/main.py
@@ -1,12 +1,64 @@
 #!/usr/bin/env python3
+
+# # Task 0. Filtered logger function
+# """
+# Main file
+# """
+
+# filter_datum = __import__('filtered_logger').filter_datum
+
+# fields = ["password", "date_of_birth"]
+# messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
+
+# for message in messages:
+#     print(filter_datum(fields, 'xxx', message, ';'))
+
+# # Task 1. Let's confirm that the log message is being obfuscated
+# """
+# Main file
+# """
+
+# import logging
+# import re
+
+# RedactingFormatter = __import__('filtered_logger').RedactingFormatter
+
+# message = "name=Bob;email=bob@dylan.com;ssn=000-123-0000;password=bobby2019;"
+# log_record = logging.LogRecord("my_logger", logging.INFO, None, None, message, None, None)
+# formatter = RedactingFormatter(fields=("email", "ssn", "password"))
+# print(formatter.format(log_record))
+
+# # Task 2. Create logger
+# #!/usr/bin/env python3
+# """
+# Main file
+# """
+
+# import logging
+
+# get_logger = __import__('filtered_logger').get_logger
+# PII_FIELDS = __import__('filtered_logger').PII_FIELDS
+
+# print(get_logger.__annotations__.get('return'))
+# print("PII_FIELDS: {}".format(len(PII_FIELDS)))
+
+
+import os
+print(os.environ.get('PERSONAL_DATA_DB_USERNAME'))
+print(os.environ.get('PERSONAL_DATA_DB_PASSWORD'))
+print(os.environ.get('PERSONAL_DATA_DB_HOST'))
+print(os.environ.get('PERSONAL_DATA_DB_NAME'))
+# Task 3. Connect to secure database
 """
 Main file
 """
 
-filter_datum = __import__('filtered_logger').filter_datum
+get_db = __import__('filtered_logger').get_db
 
-fields = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-
-for message in messages:
-    print(filter_datum(fields, 'xxx', message, ';'))
+db = get_db()
+cursor = db.cursor()
+cursor.execute("SELECT COUNT(*) FROM users;")
+for row in cursor:
+    print(row[0])
+cursor.close()
+db.close()

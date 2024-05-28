b72880b47aaed4aebf0b5eea7260674d916ee952
Olakunle64 <133286231+Olakunle64@users.noreply.github.com>
2024-05-28 16:27:50 +0100

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,7 +53,7 @@ class DB:
         except InvalidRequestError as e:
             raise e
 
-    def update_user(self, user_id, **kwargs):
+    def update_user(self, user_id: str, **kwargs):
         """update a user and return None"""
         user = self.find_user_by(id=user_id)
         for key, value in kwargs.items():

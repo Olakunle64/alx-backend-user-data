37bb94d6a42d06005b254b3c5aef77ddad43f42b
Olakunle64 <olakunleisiaq50@gmail.com>
2024-06-01 01:23:43 +0100

    change annotation in db.py file

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -53,7 +53,7 @@ class DB:
         except InvalidRequestError as e:
             raise e
 
-    def update_user(self, user_id, **kwargs):
+    def update_user(self, user_id: int, **kwargs) -> None:
         """update a user and return None"""
         user = self.find_user_by(id=user_id)
         expected_attr = user.__dict__.keys()
